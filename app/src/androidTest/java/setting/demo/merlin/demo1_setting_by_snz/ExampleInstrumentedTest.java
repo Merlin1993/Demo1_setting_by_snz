80c75e21739e58b372d2b6fa89b57187e8808606
unknown <>
2017-04-07 15:03:48 +0800

    Initial commit

---
+++ app/src/androidTest/java/setting/demo/merlin/demo1_setting_by_snz/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package setting.demo.merlin.demo1_setting_by_snz;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("setting.demo.merlin.demo1_setting_by_snz", appContext.getPackageName());
+    }
+}
